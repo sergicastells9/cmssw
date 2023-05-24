d315ae1f78660c1d4482d25ea6d0ad1a04294487
Sergi Castells <scastel2@nd.edu>
2023-05-24 17:53:09 +0200

    Working through pre-exercises

--- PhysicsTools/FWLite/bin/FWLiteHistograms.cc
+++ PhysicsTools/FWLite/bin/FWLiteHistograms.cc
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
 {
   // define what muon you are using; this is necessary as FWLite is not 
   // capable of reading edm::Views
-  using reco::Muon;
+  using pat::Muon;
 
   // ----------------------------------------------------------------------
   // First Part: 
@@ -86,7 +86,7 @@ int main(int argc, char* argv[])
 
 	// Handle to the muon collection
 	edm::Handle<std::vector<Muon> > muons;
-	event.getByLabel(std::string("muons"), muons);
+	event.getByLabel(std::string("slimmedMuons"), muons);
 	
 	// loop muon collection and fill histograms
 	for(std::vector<Muon>::const_iterator mu1=muons->begin(); mu1!=muons->end(); ++mu1){
